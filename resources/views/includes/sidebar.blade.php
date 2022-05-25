74faae9443b9fab75622a408c301fb888c940215
ruston.efendi@redtech.co.id <ruston.efendi@redtech.co.id>
2022-05-25 21:51:06 +0700

    change icon

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -7,12 +7,11 @@
                         ="menu-icon fa fa-laptop"></i>Dashboard</a>
                 </li>
 
-                <li class="menu-title">Travel Package</li>
                 <li class="menu-item-has-children dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-plane text-danger"></i>Product</a>
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-user"></i>Karyawan</a>
                     <ul class="sub-menu children dropdown-menu">
                         <li>
-                            <i class="menu-icon fa fa-map-marker"></i><a href="">Data Master</a>
+                            <i class="menu-icon fa fa-archive"></i><a href="">Data Master</a>
                         </li>
                         <li>
                             <i class="menu-icon fa fa-plus"></i><a href="">Tambah Paket Travel</a>
@@ -21,10 +20,10 @@
                 </li>
 
                 <li class="menu-item-has-children dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-plane text-danger"></i>Product</a>
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-calendar-check-o"></i>Absensi</a>
                     <ul class="sub-menu children dropdown-menu">
                         <li>
-                            <i class="menu-icon fa fa-map-marker"></i><a href="">Data Master</a>
+                            <i class="menu-icon fa fa-archive"></i><a href="">Data Master</a>
                         </li>
                         <li>
                             <i class="menu-icon fa fa-plus"></i><a href="">Tambah Paket Travel</a>
@@ -33,10 +32,10 @@
                 </li>
 
                 <li class="menu-item-has-children dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-plane text-danger"></i>Product</a>
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-credit-card"></i>Pinjaman Karyawan</a>
                     <ul class="sub-menu children dropdown-menu">
                         <li>
-                            <i class="menu-icon fa fa-map-marker"></i><a href="">Data Master</a>
+                            <i class="menu-icon fa fa-archive"></i><a href="">Data Master</a>
                         </li>
                         <li>
                             <i class="menu-icon fa fa-plus"></i><a href="">Tambah Paket Travel</a>
@@ -45,10 +44,10 @@
                 </li>
 
                 <li class="menu-item-has-children dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-plane text-danger"></i>Product</a>
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-money"></i>Penggajian</a>
                     <ul class="sub-menu children dropdown-menu">
                         <li>
-                            <i class="menu-icon fa fa-map-marker"></i><a href="">Data Master</a>
+                            <i class="menu-icon fa fa-archive"></i><a href="">Data Master</a>
                         </li>
                         <li>
                             <i class="menu-icon fa fa-plus"></i><a href="">Tambah Paket Travel</a>
@@ -57,10 +56,22 @@
                 </li>
 
                 <li class="menu-item-has-children dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-plane text-danger"></i>Product</a>
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-cog" aria-hidden="true"></i>Pengaturan</a>
+                <ul class="sub-menu children dropdown-menu">
+                    <li>
+                        <i class="menu-icon fa fa-users"></i><a href="">Manage User</a>
+                    </li>
+                    <li>
+                        <i class="menu-icon fa fa-plus"></i><a href="">Tambah Paket Travel</a>
+                    </li>
+                </ul>
+            </li>
+
+                <li class="menu-item-has-children dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-book"></i>Laporan</a>
                     <ul class="sub-menu children dropdown-menu">
                         <li>
-                            <i class="menu-icon fa fa-map-marker"></i><a href="">Data Master</a>
+                            <i class="menu-icon fa fa-archive"></i><a href="">Data Gaji</a>
                         </li>
                         <li>
                             <i class="menu-icon fa fa-plus"></i><a href="">Tambah Paket Travel</a>
